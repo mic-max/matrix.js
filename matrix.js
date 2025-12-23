648790cc180aa92daf6b14d5436988d117f4a076
Michael Maxwell <mike.ian.maxwell@gmail.com>
2025-12-23 16:23:25 -0500

    add favicon

--- matrix.js
+++ matrix.js
@@ -1,24 +1,24 @@
-var font = 10;
+const FONT_PX = 14;
 var chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 var delay = 60;
 var color = '#08f';
 var rand = 0.97;
-var cls = [0, 0, 0, 0.1];
+const CLS = [0, 0, 0, 0.1];
 
-var canvas = document.getElementById('matrix');
-var ctx = canvas.getContext('2d');
-var drops = [];
+const canvas = document.getElementById('matrix');
+const ctx = canvas.getContext('2d');
+let drops = [];
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 for(var i = 0; i < canvas.width / font; i++)
-	drops[i] = Math.random() * -8;
+	drops.push(Math.random() * -8);
 
-ctx.font = font + 'px monospace';
+ctx.font = `${FONT_PX}px monospace`;
 
 setInterval(function() {
-	ctx.fillStyle = 'rgba(' + cls.join() + ')';
+	ctx.fillStyle = 'rgba(' + CLS.join() + ')';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = color;
 
